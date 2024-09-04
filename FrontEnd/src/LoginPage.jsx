2fd294f800f9ff1aa29c564065e0b46328f328a0
Indhumalar <147085683+indhu1408@users.noreply.github.com>
2024-09-04 21:47:57 +0530

    Add files via upload

---
+++ FrontEnd/src/LoginPage.jsx
@@ -0,0 +1,13 @@
+
+
+export default function LoginPage(){
+
+    return (
+        <form className="login" action="">
+            <input type="text" placeholder="username"/>
+            <input type="text" placeholder="password"/>
+            <button>Login</button>
+        </form>
+
+    );
+}
---
+++ FrontEnd/src/Post.jsx
@@ -0,0 +1,19 @@
+export default function Post(){
+
+    return (
+    <div className="post">
+    <div className="image">
+      <img src="https://c.ndtvimg.com/2024-09/f9g6bph_deepthi-jeevanji-_625x300_04_September_24.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605" alt=''/>
+      </div>
+      <div className="text">
+      <h2> 
+      Called 'Monkey, Mental' By Fellow Villagers </h2>
+      <p className="info">
+        <a className="author">indhu</a>
+
+      </p>
+      <p>Deepthi Jeevanji bagged the 16th medal for India as she won the bronze on Tuesday in the women's 400m T20 final at the ongoing Paris Paralympics 2024.</p>
+      </div>
+      </div>
+      );
+}
